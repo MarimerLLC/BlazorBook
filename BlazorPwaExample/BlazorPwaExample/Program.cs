0f8f732f9c45ed31c9cb8bdbb6766dec513e1e71
Rockford Lhotka <rocky@lhotka.net>
2020-11-29 17:54:17 -0600

    Update to dotnet 5

--- BlazorPwaExample/BlazorPwaExample/Program.cs
+++ BlazorPwaExample/BlazorPwaExample/Program.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BlazorPwaExample
 {
@@ -15,9 +15,9 @@ namespace BlazorPwaExample
     public static async Task Main(string[] args)
     {
       var builder = WebAssemblyHostBuilder.CreateDefault(args);
-      builder.RootComponents.Add<App>("app");
+      builder.RootComponents.Add<App>("#app");
 
-      builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
       await builder.Build().RunAsync();
     }
