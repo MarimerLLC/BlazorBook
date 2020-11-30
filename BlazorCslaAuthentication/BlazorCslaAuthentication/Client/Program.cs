bc25f5856ea18388b917594733dc77e10ced60ad
Rockford Lhotka <rocky@lhotka.net>
2020-11-29 22:40:26 -0600

    Update for dotnet 5 and private prerelease of CSLA 5.4.0

--- BlazorCslaAuthentication/BlazorCslaAuthentication/Client/Program.cs
+++ BlazorCslaAuthentication/BlazorCslaAuthentication/Client/Program.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Components.Authorization;
 using Csla.Blazor.Client.Authentication;
 using Csla.Configuration;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BlazorCslaAuthentication.Client
 {
@@ -14,9 +14,9 @@ namespace BlazorCslaAuthentication.Client
     public static async Task Main(string[] args)
     {
       var builder = WebAssemblyHostBuilder.CreateDefault(args);
-      builder.RootComponents.Add<App>("app");
+      builder.RootComponents.Add<App>("#app");
 
-      builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
       builder.Services.AddOptions();
       builder.Services.AddAuthorizationCore();
@@ -27,7 +27,7 @@ namespace BlazorCslaAuthentication.Client
       builder.UseCsla(c =>
       {
         c.DataPortal()
-              .DefaultProxy(typeof(Csla.DataPortalClient.HttpProxy), "https://localhost:44317/api/dataportaltext/");
+              .DefaultProxy(typeof(Csla.DataPortalClient.HttpProxy), "https://localhost:44345/api/dataportaltext/");
       });
 
       await builder.Build().RunAsync();
