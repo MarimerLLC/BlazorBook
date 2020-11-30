2469fac70c75dc939a1ce98074f4dc61697c27eb
Rockford Lhotka <rocky@lhotka.net>
2020-11-29 18:12:43 -0600

    Update for dotnet 5

--- BlazorClientExample/BlazorClientExample/Client/Program.cs
+++ BlazorClientExample/BlazorClientExample/Client/Program.cs
@@ -1,8 +1,12 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BlazorClientExample.Client
 {
@@ -11,9 +15,9 @@ namespace BlazorClientExample.Client
     public static async Task Main(string[] args)
     {
       var builder = WebAssemblyHostBuilder.CreateDefault(args);
-      builder.RootComponents.Add<App>("app");
+      builder.RootComponents.Add<App>("#app");
 
-      builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
       await builder.Build().RunAsync();
     }
--- BlazorClientExample/BlazorClientExample/Shared/WeatherForecast.cs
+++ BlazorClientExample/BlazorClientExample/Shared/WeatherForecast.cs
@@ -4,14 +4,14 @@ using System.Text;
 
 namespace BlazorClientExample.Shared
 {
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
+  public class WeatherForecast
+  {
+    public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+    public int TemperatureC { get; set; }
 
-        public string Summary { get; set; }
+    public string Summary { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+  }
 }
